82ea4e310a7e19aeeaa8ba6d861eac3ec944055b
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-02 16:17:17 +0530

    java basics

---
+++ area.java
@@ -0,0 +1,13 @@
+class Area
+{
+    public static double area_circle(float r) 
+    {
+        return 3.14*r*r;
+        
+    }
+    public static void main (String[] args)
+    {
+
+        System.out.println(area_circle(2));
+    }
+}
---
+++ hello.java
@@ -0,0 +1,17 @@
+class Addition
+{
+    public static void main(String[] args )
+    {
+           int a=67;
+           int b=78;
+           int sum=a+b;
+           boolean result=true;
+           System.out.println("the result is " + result);
+        //    System.out.print("The sum is " + a+b  );        
+           System.out.print("The sum is " + sum  );        
+    //     System.out.print("hello world");
+    //     System.out.print("23456");
+    //     System.out.print(67);
+    //     System.out.print('S');
+    }
+}
