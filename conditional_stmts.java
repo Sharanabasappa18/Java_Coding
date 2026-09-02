893ee1351688429b8d467cd76c5027c97f16c630
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-02 23:06:44 +0530

    added if conditional stmt

---
+++ conditional_stmts.java
@@ -0,0 +1,16 @@
+// if condition
+class Vote
+{
+    public static void main(String[] args)
+    {
+        int age=18;
+        if(age<=18)
+        {
+            System.out.println("not eligible for vote");
+        }
+        else
+        {
+            System.out.println("eligible for vote");
+        }
+    }
+}
