94e7bc85af98a35058c108f49858de174c46dcc7
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-04 00:07:36 +0530

    if-else if-else condition

--- conditional_stmts.java
+++ conditional_stmts.java
@@ -14,3 +14,43 @@ class Vote
         }
     }
 }
+
+// if-else if-else 
+class conditionals
+{
+    public static void main(String[] args) {
+        String name="sharanu";
+        if (name == "sharanu")
+        {
+          System.out.println("Accesss granted");
+        }
+        else if(name=="tm")
+        {
+            System.out.println("Access granted");
+        }
+        else{
+             System.out.println("Access denied");
+        }
+    }
+}
+
+
+// if-else if-else comparing using .equals()
+class conditional
+{
+    public static void main(String[] args) {
+        String name="tm";
+
+        if(name.equals("sharanu"))
+        {
+          System.out.println("Accesss granted");
+        }
+        else if(name.equals("tm"))
+        {
+            System.out.println("Access granted");
+        }
+        else{
+            System.out.println("Access denied");
+        }
+    }
+}
