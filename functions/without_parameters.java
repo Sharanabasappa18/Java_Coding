a533ef2257dc4603fc3d869ddcdedeb61fe61448
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-06 21:49:11 +0530

    Add without paramater function

---
+++ functions/without_parameters.java
@@ -0,0 +1,11 @@
+class Demo
+{
+    public static void add() {
+        int a=12; int b=10;
+        System.out.println(a+b);
+        
+    }
+    public static void main(String[] args) {
+        add();
+    }
+}
