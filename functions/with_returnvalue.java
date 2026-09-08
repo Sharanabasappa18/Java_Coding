52d2c14b7afa326d0e45d944a342753d990f34bf
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-08 16:54:07 +0530

    add function with return value

---
+++ functions/with_returnvalue.java
@@ -0,0 +1,13 @@
+class Returnvalue
+{
+    
+    public static int multiply(int x,int y)
+    {
+            return (x*y);
+    }
+    
+    public static void main(String[] args) 
+    {
+        System.err.println(multiply(12,3));
+    }
+}
