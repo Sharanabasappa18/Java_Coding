f42179d7b60e5896f832241856446dfd5d7e8d2b
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-11 16:58:49 +0530

    add if-else if-else condition

---
+++ conditional_stmts/ifelse_ifelse.java
@@ -0,0 +1,18 @@
+// if-else if-else 
+class Conditionals
+{
+    public static void main(String[] args) {
+        String name="sharanu";
+        if(name == "sharanu")
+        {
+          System.out.println("Access granted");
+        }
+        else if(name=="tm")
+        {
+            System.out.println("Access granted");
+        }
+        else{
+             System.out.println("Access denied");
+        }
+    }
+}
