1764bcb3add08b55292d826aece924a58b8aa6c6
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-16 00:13:20 +0530

    Added program to print numbers divisible by 7 from 100 to 200

---
+++ loops/divisible.java
@@ -0,0 +1,12 @@
+class  Divisible
+{
+    public static void main(String[] args) {
+        for (int i=100;i<=200;i++)
+        {
+            if(i%7==0)  
+            {
+                System.err.println(i);
+            }
+        }
+    }
+}
