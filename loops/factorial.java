94f5582ce9eea14806baed701366722ae3b7d614
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-16 23:43:59 +0530

    Calculate factorial of a number

---
+++ loops/factorial.java
@@ -0,0 +1,11 @@
+class Multiplication
+{
+    public static void main(String[] args) {
+        int mul=1;
+        for(int i=1;i<=5;i++)
+        {
+            mul=mul*i;
+        }
+        System.out.println(mul);
+    }
+}
