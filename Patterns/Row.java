8d79b2ad16bef3200d96a1a2ddb5583b94821260
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-18 00:02:27 +0530

    Print a row of 5 stars using nested loops

---
+++ Patterns/Row.java
@@ -0,0 +1,11 @@
+class Row
+{
+    public static void main(String[] args){
+    for(int r=1; r<=1;r++){
+        for(int c=1;c<=5;c++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
