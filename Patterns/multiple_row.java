714d790997b9ecd1bb587d271aca59043a7c20af
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-19 21:51:59 +0530

    Add Multiplerow pattern program

---
+++ Patterns/multiple_row.java
@@ -0,0 +1,11 @@
+class Multiplerow
+{
+    public static void main(String[] args){
+    for(int r=1; r<=6;r++){
+        for(int c=1;c<=6;c++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
